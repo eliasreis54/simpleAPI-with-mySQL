99a4526073bfc50b2a610e4ce780c7a258573465
Elias Reis <eliasreis54@gmail.com>
2018-09-03 18:26:14 -0300

    teste 2

--- src/main/java/com/pratice2/pratice/Models/teste.java
+++ src/main/java/com/pratice2/pratice/Models/teste.java
@@ -11,18 +11,10 @@ public class teste {
 	public void setTeste(int teste) {
 		this.teste = teste;
 	}
-<<<<<<< HEAD
 	
 	@Override
 	public String toString() {
 		return "teste [teste=" + teste + "]";
 	}
-=======
 
-	@Override
-	public String toString() {
-		return "teste do toString [teste=" + teste + "]";
-	}
-	
->>>>>>> 63e60c43841c5203f56a6a5d4c7574c0f52261db
 }
