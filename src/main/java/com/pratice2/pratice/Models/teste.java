6e0902822c2f03328dc58291780f8d36fafff2ad
Elias Reis <eliasreis54@gmail.com>
2018-09-03 18:19:25 -0300

    Teste 1

--- src/main/java/com/pratice2/pratice/Models/teste.java
+++ src/main/java/com/pratice2/pratice/Models/teste.java
@@ -3,4 +3,14 @@ package com.pratice2.pratice.Models;
 public class teste {
 
 	private int teste;
+
+	public int getTeste() {
+		return teste;
+	}
+
+	public void setTeste(int teste) {
+		this.teste = teste;
+	}
+	
+	
 }
